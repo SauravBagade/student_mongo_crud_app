253d485349057c070546119774d11a011aec930f
Saurav Bagade <162048978+SauravBagade@users.noreply.github.com>
2025-11-23 13:00:33 +0530

    Change host to '0.0.0.0' for app.run

--- app.py
+++ app.py
@@ -76,4 +76,4 @@ def delete(id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
